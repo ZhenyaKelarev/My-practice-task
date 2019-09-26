3de6488cd50356ebf41e95b65800ea61c218c12a
Yevhenii Kielariev <y.kielariev@viseven.com>
2019-09-26 11:56:13 +0300

    Trying to understand pattern module

--- slider/js/script.js
+++ slider/js/script.js
@@ -1,32 +1,33 @@
+
 const slider = (function() {
-    const carouselSlide = document.querySelector(".carousel-slide");
+    let carouselSlide = document.querySelector(".carousel-slide");
+    let carouselContainer = document.querySelector(".carousel-container");
     const carouselImages = document.querySelectorAll(".carousel-slide img");
     // Buttons
-    const prevBtn = document.querySelector(".prev");
-    const nextBtn = document.querySelector(".next");
+    let prevBtn = document.querySelector(".prev");
+    let nextBtn = document.querySelector(".next");
     
     prevBtn.setAttribute("disabled", "disabled");
     prevBtn.classList.remove("active");
-    
     
     let counter = 0;
     let imageWidth = carouselImages[0].clientWidth;
     
     // events for buttons
     nextBtn.addEventListener('click', () => {
-        carouselSlide.style.transition = 'transform 0.4s ease-in-out';
-        counter++;
-        carouselSlide.style.transform = 'translateX(' + (-imageWidth * counter) + 'px)';
-        checkButtons();
+            carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+            counter++;
+            carouselSlide.style.transform = 'translateX(' + (-imageWidth * counter) + 'px)';
+            checkButtons();
     });
-    
+
     prevBtn.addEventListener('click', () => {
-        carouselSlide.style.transition = 'transform 0.4s ease-in-out';
-        counter--;
-        carouselSlide.style.transform = 'translateX(' + (-imageWidth * counter) + 'px)';
-        checkButtons();
+            carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+            counter--;
+            carouselSlide.style.transform = 'translateX(' + (-imageWidth * counter) + 'px)';
+            checkButtons();
     });
-    
+
     function checkButtons(){
         if (counter == 0) {
             prevBtn.setAttribute("disabled", "disabled");
@@ -42,21 +43,45 @@ const slider = (function() {
             prevBtn.classList.add("active");
             nextBtn.classList.add("active");
     
+        }   
+    }
+    let createSlider = function(className) {
+        const slider = document.querySelector('.slider');
+        const sliders = document.querySelector('body');
+
+        let copy = slider.cloneNode(true);
+        copy.classList.toggle(className);
+        sliders.appendChild(copy);
+        return sliders;
+    }
+    let btnProperty = function(color = "white",radius) {
+        nextBtn.style.backgroundColor = color;
+        prevBtn.style.backgroundColor = nextBtn.style.backgroundColor;
+        if(radius) {
+            nextBtn.style.borderRadius = '10px';
+            prevBtn.style.borderRadius = nextBtn.style.borderRadius;
+        } else {
+            nextBtn.style.borderRadius = '0px';
+            prevBtn.style.borderRadius = nextBtn.style.borderRadius;
         }
+        return nextBtn;
+        
+         
     }
     return {
-        createSlider: function(className) {
-            const slider = document.querySelector('.slider');
-            const sliders = document.querySelector('body');
+        btnProperty: btnProperty
+        // heightSlider : heightSlider,
+        // colorBtn : colorBtn,
+        // images : images
 
-            let copy = slider.cloneNode(true);
-            copy.classList.toggle(className);
-            sliders.appendChild(copy);
-
-        }
     }
+    
 })();
-slider.createSlider("yarik");
-slider.createSlider("yarik2");
-slider.createSlider("yarik3");
-slider.createSlider("yarik4");
+
+// slider.createSlider("yarik");
+// slider.createSlider("yarik2");
+// slider.createSlider("yarik3");
+// slider.createSlider("yarik4");
+// slider.createSlider("yarik");
+slider.btnProperty("red",true);
+
