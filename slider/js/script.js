07c75c35df48e9f2cd703fe802518cc75095f58a
Yevhenii Kielariev <y.kielariev@viseven.com>
2019-10-02 16:35:59 +0300

    slider version 3.0

--- slider/js/script.js
+++ slider/js/script.js
@@ -1,34 +1,25 @@
 
 const slider = (function() {
-    let carouselSlide = document.querySelector(".carousel-slide");
-    let carouselContainer = document.querySelector(".carousel-container");
-    const carouselImages = document.querySelectorAll(".carousel-slide img");
-    // Buttons
-    let prevBtn = document.querySelector(".prev");
-    let nextBtn = document.querySelector(".next");
-    
-    prevBtn.setAttribute("disabled", "disabled");
-    prevBtn.classList.remove("active");
-    
-    let counter = 0;
-    let imageWidth = carouselImages[0].clientWidth;
-    
-    // events for buttons
-    nextBtn.addEventListener('click', () => {
-            carouselSlide.style.transition = 'transform 0.4s ease-in-out';
-            counter++;
-            carouselSlide.style.transform = 'translateX(' + (-imageWidth * counter) + 'px)';
-            checkButtons();
-    });
+    let sliders = document.querySelector(".sliders");
 
-    prevBtn.addEventListener('click', () => {
-            carouselSlide.style.transition = 'transform 0.4s ease-in-out';
-            counter--;
-            carouselSlide.style.transform = 'translateX(' + (-imageWidth * counter) + 'px)';
-            checkButtons();
-    });
+    function clickNextBtn (counter,carouselSlide,imageWidth,prevBtn,nextBtn,carouselImages) {
+        carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+        counter++;
+        carouselSlide.style.transform = 'translateX(' + (-imageWidth * counter) + 'px)';
+        console.log("click");
+        console.log(counter);
+        checkButtons(counter,prevBtn,nextBtn,carouselImages);
+    }
+    function clickPrevBtn (counter,carouselSlide,imageWidth,prevBtn,nextBtn,carouselImages) {
+        carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+        counter--;
+        carouselSlide.style.transform = 'translateX(' + (-imageWidth * counter) + 'px)';
+        console.log("click");
+        console.log(counter);
+        checkButtons(counter,prevBtn,nextBtn,carouselImages);
+    }
 
-    function checkButtons(){
+    function checkButtons(counter,prevBtn,nextBtn,carouselImages){
         if (counter == 0) {
             prevBtn.setAttribute("disabled", "disabled");
             prevBtn.classList.remove("active");
@@ -45,43 +36,209 @@ const slider = (function() {
     
         }   
     }
-    let createSlider = function(className) {
-        const slider = document.querySelector('.slider');
-        const sliders = document.querySelector('body');
+    // ФУНКЦИЯ СОЗДАНИЯ СЛАЙДЕРА
+    let createSlider = function(classTest,img1, img2, color) {
+        let sliderDiv = document.createElement("div");
+        let carouselContainer = document.createElement("div");
+        let carouselSlide = document.createElement("div");
+        let image1 = document.createElement("img");
+        let image2 = document.createElement("img");
+        let imageWidth = image1.clientWidth;
 
-        let copy = slider.cloneNode(true);
-        copy.classList.toggle(className);
-        sliders.appendChild(copy);
-        return sliders;
+        carouselContainer.className = "carousel-container";
+        carouselSlide.className = "carousel-slide";
+        image1.setAttribute("src", img1);
+        image2.setAttribute("src", img2);
+
+        sliderDiv.className = classTest;
+
+        let carouselImages = document.querySelectorAll(".carousel-slide img");
+
+        let fragment = document.createDocumentFragment();
+        fragment.appendChild(sliderDiv);
+
+        sliders.appendChild(fragment);
+        sliderDiv.appendChild(carouselContainer);
+        carouselContainer.appendChild(carouselSlide);
+        carouselSlide.appendChild(image1);
+        carouselSlide.appendChild(image2);
+
+        createButtons(sliderDiv,color,carouselSlide,imageWidth,carouselImages);
+        return sliderDiv;
     }
-    let btnProperty = function(color = "white",radius) {
+    // ФУНКЦИЯ СОЗДАНИЯ КНОПОК ДЛЯ СЛАЙДЕРА
+    function createButtons(sliderDiv,color,carouselSlide,imageWidth,carouselImages) {
+        let prevBtn = document.createElement("button");
+        let nextBtn = document.createElement("button");
+        let buttons = document.createElement("div");
+        let prevSpan = document.createElement("span");
+        let nextSpan = document.createElement("span");
+        
+        buttons.className = "buttons";
+        nextBtn.className = "next button active";
+        prevBtn.className = "prev button active";
+
+        prevSpan.innerText = "Prev";
+        nextSpan.innerText = "Next";
+
+        prevBtn.setAttribute("disabled", "disabled");
+        prevBtn.classList.remove("active");
+
+        let counter = 0;
+        prevBtn.addEventListener('click', clickPrevBtn(counter,carouselSlide,imageWidth,prevBtn,nextBtn,carouselImages));
+        nextBtn.addEventListener('click', clickNextBtn(counter,carouselSlide,imageWidth,prevBtn,nextBtn,carouselImages));
+
+        let fragment = document.createDocumentFragment();
+
+        fragment.appendChild(buttons);
+        sliderDiv.appendChild(fragment);
+        nextBtn.appendChild(nextSpan);
+        prevBtn.appendChild(prevSpan);
+        buttons.appendChild(prevBtn);
+        buttons.appendChild(nextBtn);
+
         nextBtn.style.backgroundColor = color;
         prevBtn.style.backgroundColor = nextBtn.style.backgroundColor;
-        if(radius) {
-            nextBtn.style.borderRadius = '10px';
-            prevBtn.style.borderRadius = nextBtn.style.borderRadius;
-        } else {
-            nextBtn.style.borderRadius = '0px';
-            prevBtn.style.borderRadius = nextBtn.style.borderRadius;
-        }
+    }
+    let colorBtnFunc = function(color) {
+        nextBtn.style.backgroundColor = color;
+        prevBtn.style.backgroundColor = nextBtn.style.backgroundColor;
         return nextBtn;
-        
-         
     }
     return {
-        btnProperty: btnProperty
-        // heightSlider : heightSlider,
-        // colorBtn : colorBtn,
-        // images : images
-
-    }
-    
+        createSlider : createSlider
+    } 
 })();
+slider.createSlider("yarik","images/cat3.jpg","images/cat2.jpg","red");
 
-// slider.createSlider("yarik");
-// slider.createSlider("yarik2");
-// slider.createSlider("yarik3");
-// slider.createSlider("yarik4");
-// slider.createSlider("yarik");
-slider.btnProperty("red",true);
+
+
+
+
+
+
+// старый вариант
+// const slider = (function() {
+//     let sliders = document.querySelector(".sliders");
+//     let carouselSlide = document.querySelector(".carousel-slide");
+//     const carouselImages = document.querySelectorAll(".carousel-slide img");
+//     // Buttons
+//     let prevBtn = document.querySelector(".prev");
+//     let nextBtn = document.querySelector(".next");
+    
+//     prevBtn.setAttribute("disabled", "disabled");
+//     prevBtn.classList.remove("active");
+    
+//     let counter = 0;
+//     let imageWidth = carouselImages[0].clientWidth;
+    
+//     // events for buttons
+//     nextBtn.addEventListener('click', () => {
+//             carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+//             counter++;
+//             carouselSlide.style.transform = 'translateX(' + (-imageWidth * counter) + 'px)';
+//             checkButtons();
+//     });
+
+//     prevBtn.addEventListener('click', () => {
+//             carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+//             counter--;
+//             carouselSlide.style.transform = 'translateX(' + (-imageWidth * counter) + 'px)';
+//             checkButtons();
+//     });
+    
+
+
+//     function checkButtons(){
+//         if (counter == 0) {
+//             prevBtn.setAttribute("disabled", "disabled");
+//             prevBtn.classList.remove("active");
+    
+//         } else if(counter == carouselImages.length - 1){
+//             nextBtn.setAttribute("disabled", "disabled");
+//             nextBtn.classList.remove("active");
+    
+//         } else {
+//             prevBtn.removeAttribute("disabled");
+//             nextBtn.removeAttribute("disabled");
+//             prevBtn.classList.add("active");
+//             nextBtn.classList.add("active");
+    
+//         }   
+//     }
+//     // ФУНКЦИЯ СОЗДАНИЯ СЛАЙДЕРА
+//     let createSlider = function(classTest,img1, img2, color) {
+//         let sliderDiv = document.createElement("div");
+//         let carouselContainer = document.createElement("div");
+//         let carouselSlide = document.createElement("div");
+//         let image1 = document.createElement("img");
+//         let image2 = document.createElement("img");
+
+//         carouselContainer.className = "carousel-container";
+//         carouselSlide.className = "carousel-slide";
+//         image1.setAttribute("src", img1);
+//         image2.setAttribute("src", img2);
+
+//         sliderDiv.className = classTest;
+
+//         sliders.appendChild(sliderDiv);
+//         sliderDiv.appendChild(carouselContainer);
+//         carouselContainer.appendChild(carouselSlide);
+//         carouselSlide.appendChild(image1);
+//         carouselSlide.appendChild(image2);
+
+//         createButtons(sliderDiv,color);
+//         return sliderDiv;
+//     }
+//     // ФУНКЦИЯ СОЗДАНИЯ КНОПОК ДЛЯ СЛАЙДЕРА
+//     function createButtons(sliderDiv,color) {
+//         let prevBtn = document.createElement("button");
+//         let nextBtn = document.createElement("button");
+//         let buttons = document.createElement("div");
+//         let prevSpan = document.createElement("span");
+//         let nextSpan = document.createElement("span");
+    
+//         buttons.className = "buttons";
+//         nextBtn.className = "next button active";
+//         prevBtn.className = "prev button active";
+
+//         prevSpan.innerText = "Prev";
+//         nextSpan.innerText = "Next";
+
+//         prevBtn.setAttribute("disabled", "disabled");
+//         prevBtn.classList.remove("active");
+
+//         nextBtn.appendChild(nextSpan);
+//         prevBtn.appendChild(prevSpan);
+//         buttons.appendChild(prevBtn);
+//         buttons.appendChild(nextBtn);
+//         sliderDiv.appendChild(buttons);
+
+//         nextBtn.style.backgroundColor = color;
+//         prevBtn.style.backgroundColor = nextBtn.style.backgroundColor;
+//     }
+//     return {
+//         createSlider : createSlider
+//     } 
+//     // let btnProperty = function(color = "white",radius) {
+//     //     nextBtn.style.backgroundColor = color;
+//     //     prevBtn.style.backgroundColor = nextBtn.style.backgroundColor;
+//     //     if(radius) {
+//     //         nextBtn.style.borderRadius = '10px';
+//     //         prevBtn.style.borderRadius = nextBtn.style.borderRadius;
+//     //     } else {
+//     //         nextBtn.style.borderRadius = '0px';
+//     //         prevBtn.style.borderRadius = nextBtn.style.borderRadius;
+//     //     }
+//     //     return nextBtn;
+            
+//     // }
+//     // return {
+//     //     btnProperty: btnProperty
+//         // heightSlider : heightSlider,
+//         // colorBtn : colorBtn,
+//         // images : images
+// })();
+// slider.createSlider("yarik","images/cat3.jpg","images/cat2.jpg","red");
+
 
